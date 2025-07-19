e9acfabc4d8312d37a66f6a92bc184d4fc636e28
Sridhar Yadavilli <sridharlearning@gmail.com>
2025-07-19 18:41:43 -0400

    Initial setup with jupyter notebook

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name='document_portal',  # Replace with your package name   
+    author='Sridhar Yadavilli',  # Replace with your name    
+    version='0.1',  # Replace with your package version
+    packages=find_packages(),  # Automatically find packages in the directory
+)
